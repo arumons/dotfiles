3eca7242d8c870b782c6b72ed56459fa88428fef
arumons <takohati1@gmail.com>
2012-09-02 14:19:42 +0900

    init

---
+++ .emacs.d/init.el
@@ -0,0 +1,20 @@
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+(package-initialize)
+
+(setq-default indent-tabs-mode nil)
+(setq-default tab-width 2)
+
+(add-to-list 'load-path "~/.emacs.d/evil")
+(require 'evil)
+(evil-mode 1)
+
+(require 'auto-complete)
+(require 'auto-complete-config) 
+(global-auto-complete-mode t)
+
+(global-set-key (kbd "C-c h") 'helm-mini)
+(global-set-key (kbd "C-c f") 'helm-find-files)
+(global-set-key (kbd "C-c e") 'execute-extend-command)
+
